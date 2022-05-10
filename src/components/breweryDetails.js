d69f1787af218b077c1568f7194dfa611c577169
BC\s2100646 <Deepika.MaliniRajasekar@edu.bc.fi>
2022-05-10 23:06:45 +0300

    Detail component works and changes in data fetch lib.

--- src/components/breweryDetails.js
+++ src/components/breweryDetails.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { GetbreweryDetail } from './getBreweries'
 
-function BreweryDetails(){
-    return(
-        <div>
-            <p>Brewery detil</p>
+function BreweryDetails () {
+  let params = useParams()
+  const [breweryDetail, setBreweryDetail] = useState(null)
+
+  useEffect(() => {
+    GetbreweryDetail(params.id).then(res => {
+      console.log('inside useEf')
+      setBreweryDetail(res)
+    })
+  }, [])
+
+  if (breweryDetail) {
+    return (
+      <div>
+        <div className='m-2'>
+          <h3>
+            {' '}{breweryDetail.name}{' '}
+          </h3>
         </div>
-    );
+      </div>
+    )
+  }
 }
 
-export default BreweryDetails;
+export default BreweryDetails
